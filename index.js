22d4a79a3ca6dd32482f4314fdda3e00389bb2b2
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2024-04-02 14:45:18 +0800

    vercel

--- index.js
+++ index.js
@@ -15,13 +15,7 @@ config().then(() => {
   });
 });
 
-const corsConfig = {
-  origin: '',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+app.use(cors());
 
 app.use(express.json())
 app.use(indexRoutes)

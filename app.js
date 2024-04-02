4d6828af9dc19037cc12ecdd94bd7756e19b7f97
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2024-04-02 14:09:24 +0800

    vercel

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const config = require('./config').default;
+const config = require('./config');
 
 const indexRoutes = require('./routes/index.js')
 
@@ -8,12 +8,13 @@ const app = express();
 // Panggil fungsi untuk koneksi ke MongoDB dari config.js
 config().then(() => {
   // Lakukan apa pun yang perlu dilakukan setelah koneksi berhasil
-  const port = 3000;
+  const port = process.env.PORT || 3000;
   app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
   });
 });
 
+app.use(cors())
 app.use(express.json())
 app.use(indexRoutes)
 

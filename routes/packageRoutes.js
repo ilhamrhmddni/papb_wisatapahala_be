84d7cc885d3d6231cf8247e9a1cc99f0895d864a
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2024-04-17 15:11:46 +0800

    bug

--- routes/packageRoutes.js
+++ routes/packageRoutes.js
@@ -5,7 +5,7 @@ const packageController = require('../controllers/packageController');
 router.get('/packages', packageController.getAllPackages);
 router.get('/packages/:id', packageController.getPackageById);
 router.post('/packages', packageController.createPackage);
-router.put('/packages/update/:id', packageController.editPackage);
+router.put('/package/:id', packageController.editPackage);
 router.delete('/packages/:id', packageController.deletePackage);
 
 module.exports = router;
